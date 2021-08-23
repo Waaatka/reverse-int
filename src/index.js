f7e3e113d1188ea48d96a50b5fe114a0fefc182e
Anna Sapegina <watochka@gmail.com>
2021-08-23 20:16:15 +0300

    feat: reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const newString = Math.abs(n).toString()
+    const reversSymbol = newString.split('').reverse().join('')
+    return Number(reversSymbol)
 }
